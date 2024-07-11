7f3bf0357a4e346795fc7a31c1f40c6528c18ee0
fatma soliman <fatmacivil@hotmail.com>
2024-07-11 10:44:49 +0000

    avg

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,18 @@
+-- SQL script that creates a stored procedure ComputeAverageScoreForUser
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN user_id INT
+)
+BEGIN
+    DECLARE `average` FLOAT;
+
+    SELECT AVG(`score`) INTO `average`
+    FROM `corrections`
+    WHERE `corrections`.`user_id` = `user_id`;
+
+    UPDATE `users`
+    SET `average_score` = `average`
+    WHERE `users`.`id` = `user_id`;
+
+END//
+
+DELIMITER ;
