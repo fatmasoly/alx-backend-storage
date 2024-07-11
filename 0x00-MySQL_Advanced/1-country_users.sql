2fb4e911d287a39d3bb0c0e83e6d3abed42cb4eb
fatma soliman <fatmacivil@hotmail.com>
2024-07-11 03:28:45 +0000

    2nd

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- sql script creates table
+CREATE TABLE IF NOT EXISTS `users` (
+	`id` INT AUTO_INCREMENT,
+	`email` VARCHAR(255) NOT NULL UNIQUE,
+        `name` VARCHAR(255),
+	`country` ENUM('US', 'CO', 'TN') NOT NULL,
+	PRIMARY KEY(`id`)
+)
