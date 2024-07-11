fb59c8cb6e4f91ba2dd5a71c2823ebb969e71973
fatma soliman <fatmacivil@hotmail.com>
2024-07-11 10:07:50 +0000

    bonus

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,24 @@
+-- SQL script that creates a stored procedure AddBonus
+DELIMITER //
+
+CREATE PROCEDURE `AddBonus` (
+    IN user_id INT,
+    IN project_name VARCHAR(255),
+    IN score int 
+)
+BEGIN
+    DECLARE `project_id` INT;
+
+    SELECT `id` INTO `project_id`
+    FROM `projects`
+    WHERE `name` = project_name;
+
+    IF `project_id` IS NULL THEN
+        INSERT INTO `projects` (name) VALUES (project_name);
+        SELECT LAST_INSERT_ID() INTO project_id;
+    END IF;
+
+    INSERT INTO `corrections` (user_id, project_id, score) VALUES (user_id, project_id, score);
+END//
+
+DELIMITER ;
