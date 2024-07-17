98686852ffa038c438ff022addc261a42e49c160
fatma soliman <fatmacivil@hotmail.com>
2024-07-17 14:10:03 +0300

    get data

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -17,3 +17,18 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, type: Callable) -> Union[str, bytes, int, float]:
+        """Get the data from the cache using the key."""
+        data = self._redis.get(key)
+        if data is None:
+            raise ValueError("Key not found")
+        return type(data)
+
+    def get_str(self, key: str) -> str:
+        """Get the data from the cache using the key."""
+        return self.get(key, lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """Get the data from the cache using the key."""
+        return self.get(key, int)
